bdddfc0c701abe43f0865bb7674971c51a2bf8a6
Amitk6228 <59211195+Amitk6228@users.noreply.github.com>
2020-04-16 18:27:59 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,18 +1,12 @@
 <?php
-     $name= $_POST['name'];
-     $visitor_email= $_POST['email'];
-     $message = $_POST['message'];
-
-     $email_from='amitkr6228@gmail.com';
-     $email_subject="New Form Submission";
-     $email_body= "User Name: $name.\n". 
-                        "User Email:$visitor_email.\n". 
-                              "User Message:$message.\n";
-    
-     $to="amitk6228@gmail.com";
-     $headers="From: $email_from \r\n";
-     $headers="Reply-To: $visitor_email \r\n";
-     mail($to,$email_subject,$email_body,$headers);
-     header("Location:index.html");
-?>     
-                              
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Phone: $phone \n Email: $email \n Message: $message";
+$recipient = "amitk6228@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
